8401e1c3a58484bf22e27d7169b5ef9fc4eb106e
GeorgeSeretakos <g.seretakos@gmail.com>
2025-09-10 09:20:06 +0300

    sitemap

---
+++ next-app/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mihabodytecgreece.gr',
+  generateRobotsTxt: false, // we already created robots.txt manually
+  changefreq: 'weekly',
+  priority: 0.7,
+};
